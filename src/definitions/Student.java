982180f367570f0aa5dcf6ce64f202e27751aa6a
hp <gokul.kumar_cs19@gla.ac.in>
2020-08-24 20:21:25 +0530

    add fields for studentName in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,42 +7,12 @@
 package definitions;
 
 public class Student {
-    private String studentName;
+    private String studentFirstName;
+    private String studentMiddleName;
+    private String studentLastName;
     private long univRollNo;
     private int issuedBookNumbers;
     private Book[] bookIssued;
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public long getUnivRollNo() {
-        return univRollNo;
-    }
-
-    public void setUnivRollNo(long univRollNo) {
-        this.univRollNo = univRollNo;
-    }
-
-    public int getIssuedBookNumbers() {
-        return issuedBookNumbers;
-    }
-
-    public void setIssuedBookNumbers(int issuedBookNumbers) {
-        this.issuedBookNumbers = issuedBookNumbers;
-    }
-
-    public Book[] getBookIssued() {
-        return bookIssued;
-    }
-
-    public void setBookIssued(Book[] bookIssued) {
-        this.bookIssued = bookIssued;
-    }
 
 
 }
